2f63e99c53197331b5ac62631d08370c85075c8e
Maciej Myrta <maciej.myrta@protonmail.com>
2021-11-04 21:00:04 +0100

    Styling for both CSS color themes. Additional JS feature with differentiation of bottom info panels for mobile/desktop breakpoints. Hiding both once tasks list is empty.

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -4,25 +4,47 @@ const todoTower = document.querySelector('#todo-tower');
 const completeCheck = document.querySelectorAll('.complete-check');
 const deleteTask = document.querySelectorAll('.delete-task');
 const btnOptions = document.querySelectorAll('.btn-options');
-const clearCompleted = document.getElementById('clear-completed');
+const clearCompleted = document.querySelectorAll('.clear-completed');
+const mobilePanel = document.querySelector('.mobile-panel');
+const desktopPanel = document.querySelector('.desktop-panel');
 
-const checkAnyTasks = () => {
-    document.querySelectorAll('.todo-task').length > 0 ?
-    document.getElementById('tasks-empty').style = 'display: none' :
-    document.getElementById('tasks-empty').style = 'display: flex';
+
+// Function checks which information panel should be opened - the mobile or the desktop one
+const openProperPanel = () => {
+    window.innerWidth >= 768 ? 
+    desktopPanel.style = "display: block" :
+    mobilePanel.style = "display: block"
 };
 
+// Function wich enables or disables the information about empty tasks list
+const checkAnyTasks = () => {
+    if (document.querySelectorAll('.todo-task').length > 0) {
+        document.getElementById('tasks-empty').style = 'display: none';
+        todoTower.style = "border-radius: 8px 8px 0px 0px"
+        openProperPanel();
+    } 
+    else {
+        document.getElementById('tasks-empty').style = 'display: flex';
+        desktopPanel.style = "display: none";
+        mobilePanel.style = "display: none";
+        todoTower.style = "border-radius: 8px"
+    };
+};
+
+// Update the active (created but not completed) tasks count
 const updateCount = () => {
     let allTasks = document.querySelectorAll('.todo-task').length;
     let completedTasks = document.querySelectorAll('.completed').length;
     let value = 0;
     if (allTasks > 0) {
         value = allTasks - completedTasks;
-    }
-    document.getElementById('count').textContent = `${value}`;
+    };
+    for (count of document.querySelectorAll('.count')) {
+        count.textContent = `${value}`;
+    };
 }
 
-
+// Change the color theme on button click
 themeSwitch.addEventListener('click', () => {
     (document.body.className == "theme-dark") ? 
     (document.body.className = "theme-light") : 
@@ -46,6 +68,7 @@ input.addEventListener("keyup", function(event) {
         todoTower.appendChild(newDiv);
         event.target.value = '';
         updateCount();
+        checkAnyTasks();
     };
 });
 
@@ -57,12 +80,12 @@ todoTower.addEventListener('click', function(e) {
         if (e.target.checked) {
             task.classList.add('completed');
             updateCount();
-            // task.style = "text-decoration: line-through";
+            task.style = "text-decoration: line-through";
         }
         else {
             task.classList.remove('completed');
             updateCount();
-            // task.style = 'text-transform: none';
+            task.style = 'text-decoration: none';
         };
     };
 
@@ -79,27 +102,26 @@ for (let btn of btnOptions) {
     btn.addEventListener('click', (event) => {
         for (let btn of btnOptions) {
             btn.classList.remove("active");
-        }
+        };
         event.target.classList.add("active");
-    })
+    });
 };
-
 
 // Display All/Active/Completed tasks in the tower
 for (let option of btnOptions) {
     option.addEventListener('click', (event) => {
         for (let task of document.querySelectorAll('.todo-task')) {
-            if (event.target.id == "btn-all") {
+            if (event.target.classList.contains('btn-all')) {
                 task.style = "display: flex";
             };
     
-            if (event.target.id == "btn-active") {
+            if (event.target.classList.contains('btn-active')) {
                 task.classList.contains('completed') ? 
                 task.style = "display: none" :
                 task.style = "display: flex";
             };
     
-            if (event.target.id == "btn-completed") {
+            if (event.target.classList.contains('btn-completed')) {
                 task.classList.contains('completed') ? 
                 task.style = "display: flex" :
                 task.style = "display: none";
@@ -109,13 +131,41 @@ for (let option of btnOptions) {
 };
 
 // Delete tasks marked as 'Completed'
-clearCompleted.addEventListener('click', () => {
-    for (let task of document.querySelectorAll('.todo-task')) {
-        if (task.classList.contains('completed')) {
-            task.parentNode.removeChild(task);
-            checkAnyTasks();
-            updateCount();
+for(let clear of clearCompleted) {
+    clear.addEventListener('click', () => {
+        for (let task of document.querySelectorAll('.todo-task')) {
+            if (task.classList.contains('completed')) {
+                task.parentNode.removeChild(task);
+                checkAnyTasks();
+                updateCount();
+            };
         };
+    });
+};
+
+// Function to switch the information panel if user resizes the window width;
+let initialWidth = window.innerWidth;
+
+window.addEventListener("resize", () => {
+
+    if (window.innerWidth != initialWidth) {
+
+        if (window.innerWidth < 768) {
+            if (desktopPanel.style.display == "block") {
+                desktopPanel.style = "display: none";
+                mobilePanel.style = "display: block";
+            }
+        }
+
+        else {
+            if (mobilePanel.style.display == "block") {
+                mobilePanel.style = "display: none";
+                desktopPanel.style = "display: block";
+            }
+        };
+
+        initialWidth = window.innerWidth;
     };
 });
+
 
